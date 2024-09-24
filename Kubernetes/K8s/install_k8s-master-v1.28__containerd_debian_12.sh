11d8111e077338709b04d198cc594e2a022235e1
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2024-09-24 23:37:26 +0300

    Update install_k8s-master-v1.28__containerd_debian_12.sh

--- Kubernetes/K8s/install_k8s-master-v1.28__containerd_debian_12.sh
+++ Kubernetes/K8s/install_k8s-master-v1.28__containerd_debian_12.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]; then
+    exec sudo "$0" "$@"
+fi
+
 INSTALL_HELM=false
 HELM_ONLY=false
 
@@ -7,202 +11,215 @@ show_help() {
     echo "Usage: $0 [OPTIONS]"
     echo
     echo "Options:"
-    echo "  --install-helm      Automatically install Helm after setting up the Kubernetes cluster."
-    echo "  --helm-only         Only install Helm, skip Kubernetes setup."
-    echo "  --help              Display this help message."
+    echo "  -wh, --with-helm         Automatically install Helm after setting up the Kubernetes cluster."
+    echo "  -oh, --only-helm         Only install Helm, skip Kubernetes setup."
+    echo "  -h, --help              Display this help message."
     echo
     echo "Description:"
     echo "  This script sets up a Kubernetes cluster, initializes it with kubeadm,"
     echo "  configures kubectl for the user, and applies the Calico network plugin."
-    echo "  Optionally, it can also install Helm based on user input or the --install-helm flag."
+    echo "  Optionally, it can also install Helm based on user input or the --with-helm flag."
     echo "  The --helm-only flag skips Kubernetes setup and only installs Helm."
 }
 
-
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --install-helm) 
-			INSTALL_HELM=true
-			;;
-        --helm-only) HELM_ONLY=true ;;
-        --help) show_help; exit 0 ;;
-        *) echo "Unknown parameter passed: $1"; show_help; exit 1 ;;
+        -wh|--with-helm)
+            INSTALL_HELM=true
+            ;;
+        
+        -oh|--only-helm)
+            HELM_ONLY=true
+            ;;
+        
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        
+        *) 
+            echo "Unknown parameter passed: $1"
+            show_help
+            exit 1
+            ;;
     esac
     shift
 done
 
-disable_swap() {
-	echo "Disabling Swap..."
-	sudo swapoff -a
 
-	echo "Commenting swap line in /etc/fstab..."
-	sudo sed -i '/ swap / s/^/# /' /etc/fstab
+disable_swap() {
+	echo -e "[ ... ] Disabling Swap..."
+	sudo swapoff -a > /dev/null 2>&1
+	sudo sed -i '/ swap / s/^/# /' /etc/fstab > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Swap Disabled."
+	
 }
 
 install_dependencies() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Installing dependencies..."
-	sudo apt update && sudo apt install -y ufw apt-transport-https ca-certificates curl gnupg lsb-release socat conntrack
+	echo "[ ... ] Installing dependencies..."
+	sudo apt update > /dev/null 2>&1 && sudo apt install -y ufw apt-transport-https ca-certificates curl gnupg lsb-release socat conntrack > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Dependencies installed successfully."
 
 }
 
 open_ports() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Opening necessary ports..."
+	echo "[ ... ] Opening necessary ports..."
 	
-	sudo ufw allow 6443/tcp   		# Kubernetes API server
-	sudo ufw allow 2379:2380/tcp  	# etcd server client API
-	sudo ufw allow 10250/tcp  		# Kubelet API
-	sudo ufw allow 10259/tcp  		# kube-scheduler
-	sudo ufw allow 10257/tcp  		# kube-controller-manager
+	sudo ufw allow 6443/tcp > /dev/null 2>&1   		# Kubernetes API server
+	sudo ufw allow 2379:2380/tcp > /dev/null 2>&1  	# etcd server client API
+	sudo ufw allow 10250/tcp > /dev/null 2>&1  		# Kubelet API
+	sudo ufw allow 10259/tcp > /dev/null 2>&1  		# kube-scheduler
+	sudo ufw allow 10257/tcp > /dev/null 2>&1  		# kube-controller-manager
+	
+	sudo ufw --force enable > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Ports opened."
 }
 
 config_sysctl_params() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Config_Sysctl_Params: Loading necessary kernel modules..."
+	echo "[ ... ] Loading necessary kernel modules..."
 
-	sudo modprobe overlay
-	sudo modprobe br_netfilter
+	sudo modprobe overlay > /dev/null 2>&1
+	sudo modprobe br_netfilter > /dev/null 2>&1
 	
-	sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+	sudo tee /etc/sysctl.d/kubernetes.conf > /dev/null <<EOF
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 EOF
 	
-	echo "Config_Sysctl_Params: Applying sysctl params..."
-	sudo sysctl --system
+	echo -e " [\033[32m + \033[0m]  Modules loaded."
+	
+	echo "[ ... ] Applying sysctl params..."
+	sudo sysctl --system > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Sysctl applied."
 }
 
 install_containerd() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Installing containerd..."
-	
-	sudo apt install -y containerd
+	echo "[ ... ] Installing containerd..."
+	sudo apt install -y containerd > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  containerd installed."
 
-	echo "Configuring containerd..."
+	echo "[ ... ] Configuring containerd..."
 	sudo mkdir -p /etc/containerd
-	sudo containerd config default | sudo tee /etc/containerd/config.toml
+	sudo containerd config default | sudo tee /etc/containerd/config.toml > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  containerd configured."
 
-	echo "Setting cgroup driver to systemd in containerd config..."
-	sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
-
-	echo "Restarting containerd..."
+	echo "[ ... ] Setting cgroup driver to systemd in containerd config..."
+	sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Configured cgroup driver."
+	
+	echo "[ ... ] Restarting containerd..."
 	sudo systemctl restart containerd
-	sudo systemctl enable containerd
+	sudo systemctl enable containerd > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  containerd restarted successfully."
 }
 
 pre_k8s_cleanup() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Cleaning up existing Kubernetes repository entries..."
+	echo "[ ... ] Cleaning up existing Kubernetes repository entries..."
 	
 	sudo rm -f /etc/apt/sources.list.d/kubernetes.list
 	sudo rm -f /etc/apt/sources.list.d/google-cloud-sdk.list
-	sudo sed -i '/kubernetes/d' /etc/apt/sources.list
-	sudo sed -i '/packages.cloud.google.com/d' /etc/apt/sources.list
+	sudo sed -i '/kubernetes/d' /etc/apt/sources.list > /dev/null 2>&1
+	sudo sed -i '/packages.cloud.google.com/d' /etc/apt/sources.list > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Cleaning up existing Kubernetes repository entries"
 }
 
 update_signing_key() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Updating signing key..."
+	echo "[ ... ] Updating signing key..."
 	
 	sudo mkdir -p /etc/apt/keyrings
-	curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+	curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Updating signing key."
 }
 
 add_k8s_repo() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Adding Kubernetes apt repository..."
+	echo "[ ... ] Adding Kubernetes apt repository..."
 	
-	echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+	echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Adding Kubernetes apt repository."
 }
 
 install_k8s() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Installing kubelet, kubeadm, kubectl..."
+	echo "[ ... ] Installing kubelet, kubeadm, kubectl..."
+	sudo apt-get update > /dev/null 2>&1
+	sudo apt-get install -y kubelet kubeadm kubectl > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  kubelet, kubeadm, kubectl installed successfully."
 	
-	sudo apt-get update
-	sudo apt-get install -y kubelet kubeadm kubectl
+	echo "[ ... ] Locking current version of kubelet, kubeadm, kubectl..."
+	sudo apt-mark hold kubelet kubeadm kubectl > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Version locked."
 
-	echo "Install_K8s: Locking current version of kubelet, kubeadm, kubectl..."
-	sudo apt-mark hold kubelet kubeadm kubectl
-
-	echo "Install_K8s: Enabling and starting kubelet service..."
-	sudo systemctl enable --now kubelet
+	echo "[ ... ] Enabling and starting kubelet service..."
+	sudo systemctl enable --now kubelet > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Kubelet service enabled successfully."
 }
 
 config_k8s() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Configuring kubelet cgroup driver to systemd..."
+	echo "[ ... ] Configuring kubelet cgroup driver to systemd..."
 	
 	sudo mkdir -p /etc/systemd/system/kubelet.service.d
-	cat <<EOF | sudo tee /etc/systemd/system/kubelet.service.d/20-containerd.conf
+	cat <<EOF | sudo tee /etc/systemd/system/kubelet.service.d/20-containerd.conf  > /dev/null 2>&1
 [Service]
 Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --container-runtime-endpoint=unix:///run/containerd/containerd.sock"
 EOF
 
 	sudo mkdir -p /var/lib/kubelet
-	cat <<EOF | sudo tee /var/lib/kubelet/config.yaml
+	cat <<EOF | sudo tee /var/lib/kubelet/config.yaml > /dev/null 2>&1
 kind: KubeletConfiguration
 apiVersion: kubelet.config.k8s.io/v1beta1
 cgroupDriver: systemd
 EOF
 	
-	echo "config_k8s: Configuring newer version to 'pause'..."
-	sudo sed -i 's/sandbox_image = "registry\.k8s\.io\/pause:.*"/sandbox_image = "registry.k8s.io\/pause:3.9"/' /etc/containerd/config.toml
+	echo -e " [\033[32m + \033[0m]  Kubelet cgroup driver configured."
+	
+	echo "[ ... ] Configuring newer version to 'pause'..."
+	sudo sed -i 's/sandbox_image = "registry\.k8s\.io\/pause:.*"/sandbox_image = "registry.k8s.io\/pause:3.9"/' /etc/containerd/config.toml > /dev/null 2>&1
 	sudo systemctl restart containerd
-	grep "sandbox_image" /etc/containerd/config.toml
+	echo -e " [\033[32m + \033[0m]  Configured newer version to 'pause'."
 
 }
 
 k8s_reload_restart() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Reloading and restarting kubelet..."
-	
+	echo "[ ... ] Reloading and restarting kubelet..."
 	sudo systemctl daemon-reload
 	sudo systemctl restart kubelet
+	echo -e " [\033[32m + \033[0m]  Reload and restart completed."
 }
 
 init_k8s_cluster() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Initializing Kubernetes cluster with kubeadm..."
+	echo "[ ... ] Initializing Kubernetes cluster with kubeadm..."
 	
 	sudo kubeadm init --apiserver-advertise-address=$(hostname -I | awk '{print $1}') \
 	--pod-network-cidr=192.168.100.0/16
+	
+	echo ""
+	echo -e " [\033[32m + \033[0m]  Kubernetes cluster initiated successfully."
 }
 
 setup_local_k8s() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Setting up local kubeconfig..."
+	echo "[ ... ] Setting up local kubeconfig..."
 	
 	mkdir -p $HOME/.kube
 	sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-	sudo chown $(id -u):$(id -g) $HOME/.kube/config
+	sudo chown $(id -u):$(id -g) $HOME/.kube/config > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Local kubeconfig setup completed."
 }
 
 apply_calico() {
-	if [ "$1" == "silent" ]; then
-		echo -e "\n$(printf '=%.0s' {1..140})\n"
-		echo "Applying Calico network plugin [Round 2]..."
-		kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/calico.yaml > /dev/null
-	
-	else
-		echo -e "\n$(printf '=%.0s' {1..140})\n"
-		echo "Applying Calico network plugin [Round 1]..."
-		kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/calico.yaml
-	
-	fi
+	echo "[ ... ] Applying Calico network plugin..."
+	kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/calico.yaml > /dev/null 2>&1
+	echo -e " [\033[32m + \033[0m]  Calico network plugin applied."
 }
 
-display_pods_and_nodes() {
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
+display() {
+	echo -e "$(printf '=%.0s' {1..140})\n"
 	echo "CURRENT NODES >> "
 	kubectl get nodes
 	echo ""
 	
 	echo "CURRENT PODS >> "
 	kubectl get pods -n kube-system
+	echo -e "$(printf '=%.0s' {1..140})\n"
 }
-
 
 are_all_nodes_ready() {
     local max_retries=5
@@ -308,160 +325,136 @@ monitor() {
 	done
 }
 
-install_helm() {
-	install_curl_package() {
-		sudo apt-get update
-		sudo apt-get install -y curl
-	}
-	
-	install_git_package() {
-		sudo apt-get update
-		sudo apt-get install -y git
-	}
-	
-    local reason=""
-    local helm_install_script_url="https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3"
-    local helm_install_script="get_helm.sh"
-
-    printf '=%.0s' {1..140}
-    echo
-    echo "Installing Helm..."
-	if ! command -v curl >/dev/null 2>&1; then
-		echo "curl is not installed. Installing curl..."
-		install_curl_package
-		
-	fi
-
-    if ! command -v git >/dev/null 2>&1; then
-        echo "Git is not installed. Installing Git..."
-        install_git_package
-		
+manage_helm() {	
+    if helm version >/dev/null 2>&1; then
+        echo "Helm is already installed."
+        return 0
     fi
 
-    if curl -fsSL -o "$helm_install_script" "$helm_install_script_url"; then
-        chmod +x "$helm_install_script"
-
-        if ./"$helm_install_script"; then
-            if helm version >/dev/null 2>&1; then
-                printf '=%.0s' {1..140}
-                echo "HELM Version:"
-				helm version
-				
-                echo "Helm installation completed."
-                reason="Helm installation succeeded."
-                echo "$reason"
-				
-                rm -f "$helm_install_script"
-                return 0
-				
-            else
-                reason="Helm installation failed during verification."
-                echo "$reason"
-                rm -f "$helm_install_script"
-                return 1
-				
-            fi
+    if [ "$INSTALL_HELM" = true ]; then
+        install_helm
+        helm_install_status=$?
+        
+        if [ $helm_install_status -eq 0 ]; then
+            echo -e "[\033[32m + \033[0m] Helm was successfully installed."
+            return 0
 			
         else
-            reason="Helm installation script failed to execute."
-            echo "$reason"
-            rm -f "$helm_install_script"
+            echo -e "[\033[31m - \033[0m] Helm installation failed."
             return 1
 			
         fi
 		
     else
-        reason="Helm installation script failed to download."
-        echo "$reason"
+        echo "Helm installation was skipped by the user."
+        return 2
+		
+    fi
+}
+
+install_helm() {
+    install_curl_package() {
+        sudo apt-get update
+        sudo apt-get install -y curl
+    }
+    
+    install_git_package() {
+        sudo apt-get update
+        sudo apt-get install -y git
+    }
+
+    local helm_install_script_url="https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3"
+    local helm_install_script="get_helm.sh"
+
+    printf '=%.0s' {1..140}
+    echo
+    echo "[ ... ] Installing Helm..."
+    
+    if ! command -v curl >/dev/null 2>&1; then
+        echo "[ ... ] curl is not installed. Installing curl..."
+        install_curl_package > /dev/null 2>&1
+		echo -e " [\033[32m + \033[0m] curl was successfully installed."
+    fi
+
+    if ! command -v git >/dev/null 2>&1; then
+        echo "[ ... ] git is not installed. Installing Git..."
+        install_git_package > /dev/null 2>&1
+		echo -e " [\033[32m + \033[0m] git was successfully installed."
+    fi
+
+    if curl -fsSL -o "$helm_install_script" "$helm_install_script_url" > /dev/null 2>&1; then
+        chmod +x "$helm_install_script"
+        
+        if ./"$helm_install_script" > /dev/null 2>&1; then
+            if helm version >/dev/null 2>&1; then
+                rm -f "$helm_install_script"
+                return 0
+				
+            else
+                echo -e " [\033[31m - \033[0m]  Helm installation failed during verification."
+                return 1
+				
+            fi
+			
+        else
+            echo -e " [\033[31m - \033[0m]  Helm installation script failed during verification."
+            return 1
+			
+        fi
+		
+    else
+        echo -e " [\033[31m - \033[0m]  Helm installation script failed to download."
         return 1
 		
     fi
 }
 
-check_helm() {
-    local reason=""
-
-    if helm version >/dev/null 2>&1; then
-        reason="Helm is already installed."
-        echo "$reason"
-        return 0
-		
-    fi
-
-    if [ "$INSTALL_HELM" = true ]; then
-        install_helm
-        return $?
-		
-    else
-		return 2
-        
-    fi
-}
-
 
 main() {
-	if [ "$HELM_ONLY" = true ]; then
-		echo "HELM_ONLY flag is set. Skipping Kubernetes setup and only installing Helm."
+    if [ "$HELM_ONLY" = true ]; then
 		install_helm
 		helm_install_status=$?
 
 		if [ $helm_install_status -eq 0 ]; then
-			echo "Helm was successfully installed."
+			# Green color for success
+			echo -e "[\033[32m + \033[0m] Helm was successfully installed."
 			
 		else
-			echo "Helm installation failed."
+			# Red color for failure
+			echo -e "[\033[31m - \033[0m] Helm installation failed."
 			
 		fi
 
 		exit $helm_install_status
 	fi
 
-	disable_swap
-	install_dependencies
-	open_ports
-	config_sysctl_params
-	install_containerd
-	pre_k8s_cleanup
-	update_signing_key
-	add_k8s_repo
-	install_k8s
-	config_k8s
-	k8s_reload_restart
-	init_k8s_cluster
-	setup_local_k8s
-	apply_calico
-	monitor
-	display_pods_and_nodes
-	apply_calico "silent"
-	
-	
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	check_helm_output=$(check_helm)
-	check_helm_status=$?
-		
-	if [ $check_helm_status -eq 0 ]; then
-		echo "Helm was successfully installed."
-		
-	else
-		echo "$check_helm_output"
-		if [ $check_helm_status -eq 2 ]; then
-			echo "Helm installation was skipped by the user."
-		
-		else
-			echo "Helm installation failed."
-		
-		fi
-		
-	fi
-	
-	echo -e "\n$(printf '=%.0s' {1..140})\n"
-	echo "Use the following command to create new joining commands:"
-	echo "kubeadm token create --print-join-command"
-	echo ""
-	echo "If you want to install helm in a later stage you can run:"
-	echo "$0 --helm-only"
-	echo ""
-}
 
+    disable_swap
+    install_dependencies
+    open_ports
+    config_sysctl_params
+    install_containerd
+    pre_k8s_cleanup
+    update_signing_key
+    add_k8s_repo
+    install_k8s
+    config_k8s
+    k8s_reload_restart
+    init_k8s_cluster
+    setup_local_k8s
+    apply_calico
+    monitor
+    display
+
+    manage_helm
+
+    echo -e "\n$(printf '=%.0s' {1..140})\n"
+    echo "Use the following command to create new joining commands:"
+    echo "sudo kubeadm token create --print-join-command"
+    echo ""
+    echo "If you want to install helm in a later stage you can run:"
+    echo "sudo $0 -oh"
+    echo ""
+}
 
 main
