bf9b3acf151dae0209106652c9564280cafc5ae5
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2024-09-28 04:58:14 +0300

    Update install_k8s.sh

--- Kubernetes/K8s/install_k8s.sh
+++ Kubernetes/K8s/install_k8s.sh
@@ -7,7 +7,6 @@
 			
 		Software:
 			curl, sudo - set no passwd for user & usermod to sudo group
-
 		If the machine will be a CI/CD agent:
 			- add current user to sudo group:
 				sudo usermod -aG sudo $(whoami)
@@ -18,16 +17,18 @@
 			- Install git docker-ce to install agent pre-dependencies.
 			- start docker service
 			- login to dockerhub account (if exists)
+			
+	TODO:
+		1. Set parameter for time to wait between new worker node scanning (default=60)
+			
 COMMENT
 
 if [ "$EUID" -ne 0 ]; then
-	exec sudo "$0" "$@"
+    exec sudo "$0" "$@"
 fi
-	
+
 SCRIPT_DIR=$(dirname "$0")
 LISTENER_NODE="$SCRIPT_DIR/node_listener.yml"
-LISTENER_DEFAULT_TIMER=60
-MODIFY_CONTROL_PLANE=false
 IS_MASTER=false
 INSTALL_HELM=false
 HELM_ONLY=false
@@ -39,21 +40,17 @@ show_help() {
     printf "Default setting is set to install a WORKER node.\n\n"
     printf "Options:\n"
     printf "  -h, --help                       			Display this help message.\n"
-    printf "  -m, --master 						Install K8s master.\n"
+	printf "  -m, --master 						Install K8s master\n"
     printf "  -wh, --with-helm                	 		Automatically install Helm after setting up the Kubernetes cluster.\n"
     printf "  -oh, --only-helm                 			Only install Helm, skip Kubernetes setup.\n"
-    printf "  -aa, --apiserver-advertise-address [IP] 		Specify the IP address for the Kubernetes API server.\n"
-    printf "  -st, --sleep-time [SECONDS]        			Specify the sleep time (in seconds) for the node listener between checks. Default is 60 seconds.\n"
-    printf "  -mcp, --modify-control-plane      			Allow the node listener to modify control-plane nodes (default is to skip control-plane nodes).\n\n"
+    printf "  -aa, --apiserver-advertise-address [IP] 		Specify the IP address for the Kubernetes API server.\n\n"
     printf "Description:\n"
     printf "  This script sets up a Kubernetes cluster, initializes it with kubeadm,\n"
     printf "  configures kubectl for the user, and applies the Calico network plugin.\n"
     printf "  Optionally, it can also install Helm based on user input or the --with-helm flag.\n"
     printf "  The --only-helm flag skips Kubernetes setup and only installs Helm.\n"
     printf "  You can specify the API server's IP address, and the pod network CIDR will be\n"
-    printf "  automatically generated based on that IP.\n"
-    printf "  Additionally, you can customize the behavior of the node listener using the\n"
-    printf "  --sleep-time and --modify-control-plane flags.\n"
+    printf "  automatically generated based on that IP.\n\n"
 }
 
 echo_message() {
@@ -90,12 +87,12 @@ echo_message() {
 validate_ip() {
     local ip="$1"
     IFS='.' read -r -a octets <<< "$ip"
-    
+
     if [[ ${#octets[@]} -ne 4 ]]; then
 		echo_message ERROR "Error: Invalid IP address format."
         exit 1
     fi
-    
+
     for i in "${!octets[@]}"; do
         if ! [[ "${octets[$i]}" =~ ^[0-9]+$ ]] || [[ "${octets[$i]}" -lt 0 ]] || [[ "${octets[$i]}" -gt 255 ]]; then
             echo_message ERROR "Error: Invalid IP address. Each octet must be between 0 and 255."
@@ -117,43 +114,30 @@ calculate_cidr() {
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -m|--master)
-            IS_MASTER=true
-            ;;
-        
+		-m|--master)
+			IS_MASTER=true
+			;;
+
         -wh|--with-helm)
             INSTALL_HELM=true
             ;;
-        
+
         -oh|--only-helm)
             HELM_ONLY=true
             ;;
-        
+
         -aa|--apiserver-advertise-address)
             APISERVER_ADVERTISE_ADDRESS="$2"
             validate_ip "$APISERVER_ADVERTISE_ADDRESS"
             calculate_cidr "$APISERVER_ADVERTISE_ADDRESS"
             shift
             ;;
-        
-        -st|--sleep-time)
-            LISTENER_DEFAULT_TIMER="$2"
-            if ! [[ "$LISTENER_DEFAULT_TIMER" =~ ^[0-9]+$ ]]; then
-                echo_message ERROR "Invalid sleep time. It must be a positive integer."
-                exit 1
-            fi
-            shift
-            ;;
-        
-        -mcp|--modify-control-plane)
-            MODIFY_CONTROL_PLANE=true
-            ;;
-        
+
         -h|--help)
             show_help
             exit 0
             ;;
-        
+
         *) 
             echo_message ERROR "Unknown parameter passed: $1"
             show_help
@@ -177,7 +161,6 @@ disable_swap() {
             return 1
         fi
     fi
-
     if grep -E '\s+swap\s+' /etc/fstab > /dev/null; then
         if sudo sed -i '/ swap / s/^/# /' /etc/fstab > /dev/null 2>&1; then
             echo_message SUCCESS "Swap entry in /etc/fstab commented out successfully."
@@ -191,7 +174,6 @@ disable_swap() {
         echo_message ERROR "No swap entry found in /etc/fstab."
 		
     fi
-
     echo_message SUCCESS "Swap Disabled."
 }
 
@@ -199,6 +181,7 @@ install_dependencies() {
 	echo_message INFO "Installing dependencies..."
 	sudo apt update > /dev/null 2>&1 && sudo apt install -y ufw apt-transport-https ca-certificates curl gnupg lsb-release socat conntrack > /dev/null 2>&1
 	echo_message SUCCESS "Dependencies installed successfully."
+
 }
 
 open_ports() {
@@ -208,7 +191,7 @@ open_ports() {
 	sudo ufw allow 10250/tcp > /dev/null 2>&1  		# Kubelet API
 	sudo ufw allow 10259/tcp > /dev/null 2>&1  		# kube-scheduler
 	sudo ufw allow 10257/tcp > /dev/null 2>&1  		# kube-controller-manager
-	
+
 	sudo ufw --force enable > /dev/null
 	echo_message SUCCESS "Ports opened."
 }
@@ -218,14 +201,14 @@ config_sysctl_params() {
 
 	sudo modprobe overlay > /dev/null
 	sudo modprobe br_netfilter > /dev/null
-	
+
 	sudo tee /etc/sysctl.d/kubernetes.conf > /dev/null <<EOF
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 EOF
-	
+
 	echo_message SUCCESS "Kernel modules loaded."
-	
+
 	echo_message INFO "Applying sysctl params..."
 	sudo sysctl --system > /dev/null
 	echo_message SUCCESS "Sysctl applied successfully."
@@ -244,7 +227,7 @@ install_containerd() {
 	echo_message INFO "Setting cgroup driver to systemd in containerd config..."
 	sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml > /dev/null
 	echo_message SUCCESS "Configured cgroup driver."
-	
+
 	echo_message INFO "Restarting containerd..."
 	sudo systemctl restart containerd
 	sudo systemctl enable containerd > /dev/null
@@ -253,7 +236,7 @@ install_containerd() {
 
 pre_k8s_cleanup() {
 	echo_message INFO "Cleaning up existing Kubernetes repository entries..."
-	
+
 	sudo rm -f /etc/apt/sources.list.d/kubernetes.list
 	sudo rm -f /etc/apt/sources.list.d/google-cloud-sdk.list
 	sudo sed -i '/kubernetes/d' /etc/apt/sources.list > /dev/null 2>&1
@@ -263,7 +246,7 @@ pre_k8s_cleanup() {
 
 update_signing_key() {
 	echo_message INFO "Updating signing key..."
-	
+
 	sudo mkdir -p /etc/apt/keyrings
 	curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg > /dev/null 2>&1
 	echo_message SUCCESS "Updating signing key."
@@ -271,7 +254,7 @@ update_signing_key() {
 
 add_k8s_repo() {
 	echo_message INFO "Adding Kubernetes apt repository..."
-	
+
 	echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
 	echo_message SUCCESS "Added Kubernetes apt repository."
 }
@@ -281,7 +264,7 @@ install_k8s() {
 	sudo apt-get update > /dev/null 2>&1
 	sudo apt-get install -y kubelet kubeadm kubectl > /dev/null 2>&1
 	echo_message SUCCESS "kubelet, kubeadm, kubectl installed successfully."
-	
+
 	echo_message INFO "Locking current version of kubelet, kubeadm, kubectl..."
 	sudo apt-mark hold kubelet kubeadm kubectl > /dev/null
 	echo_message SUCCESS "Version locked."
@@ -293,7 +276,7 @@ install_k8s() {
 
 config_k8s() {
 	echo_message INFO "Configuring kubelet cgroup driver to systemd..."
-	
+
 	sudo mkdir -p /etc/systemd/system/kubelet.service.d
 	cat <<EOF | sudo tee /etc/systemd/system/kubelet.service.d/20-containerd.conf  > /dev/null
 [Service]
@@ -306,9 +289,9 @@ kind: KubeletConfiguration
 apiVersion: kubelet.config.k8s.io/v1beta1
 cgroupDriver: systemd
 EOF
-	
+
 	echo_message SUCCESS "Kubelet cgroup driver configured."
-	
+
 	echo_message INFO "Configuring newer version to 'pause'..."
 	sudo sed -i 's/sandbox_image = "registry\.k8s\.io\/pause:.*"/sandbox_image = "registry.k8s.io\/pause:3.9"/' /etc/containerd/config.toml > /dev/null
 	sudo systemctl restart containerd
@@ -325,7 +308,7 @@ k8s_reload_restart() {
 
 init_k8s_cluster() {
 	echo_message INFO "Initializing Kubernetes cluster with kubeadm..."
-	
+
     if [ -z "$APISERVER_ADVERTISE_ADDRESS" ]; then
         APISERVER_ADVERTISE_ADDRESS=$(hostname -I | awk '{print $1}')
 		calculate_cidr $APISERVER_ADVERTISE_ADDRESS
@@ -334,10 +317,10 @@ init_k8s_cluster() {
     if [ -z "$POD_NETWORK_CIDR" ]; then
 		calculate_cidr $APISERVER_ADVERTISE_ADDRESS
     fi
-	
+
 	echo_message DEBUG "API server advertise address: ${APISERVER_ADVERTISE_ADDRESS} | Pod Network CIDR: ${POD_NETWORK_CIDR}"
     sudo kubeadm init --apiserver-advertise-address="$APISERVER_ADVERTISE_ADDRESS" --pod-network-cidr="$POD_NETWORK_CIDR" -v=0
-	
+
 	echo ""
 	echo_message SUCCESS "Kubernetes cluster initiated successfully."
 }
@@ -362,7 +345,7 @@ display() {
 	echo "CURRENT NODES >> "
 	kubectl get nodes
 	echo ""
-	
+
 	echo "CLUSTER PODS >> "
 	kubectl get pods -A
 	echo ""
@@ -380,7 +363,7 @@ are_all_nodes_ready() {
 			echo_message ERROR "Attempt $attempt/$max_retries failed: Unable to get nodes status."
             attempt=$((attempt+1))
             sleep $retry_delay
-			
+
         else
             local all_ready=true
             for status in $nodes; do
@@ -388,12 +371,12 @@ are_all_nodes_ready() {
                     all_ready=false
                     break
                 fi
-				
+
             done
 
             if [ "$all_ready" = true ]; then
                 return 0
-				
+
             fi
 
             spin "Nodes not ready. Waiting..." &
@@ -418,7 +401,7 @@ are_all_system_pods_ready() {
 			echo_message ERROR "Attempt $attempt/$max_retries failed: Unable to get kube-system pods."
             attempt=$((attempt+1))
             sleep $retry_delay
-			
+
         else
             local all_running=true
             echo "$pods" | awk '{split($2, a, "/"); if (a[1] != a[2] || $3 != "Running") exit 1}' || all_running=false
@@ -460,7 +443,7 @@ monitor() {
 			printf "\n"
 
 			break
-			
+
 		else
 			spin "Waiting for nodes and system pods to become ready..." &
 			SPIN_PID=$!
@@ -504,7 +487,7 @@ roleRef:
   apiGroup: rbac.authorization.k8s.io
 EOF
 
-	echo_message SUCCESS "Service created successfully."
+	echo_message INFO "Service created successfully."
 }
 
 create_node_listener() {
@@ -515,38 +498,30 @@ kind: Pod
 metadata:
   name: node-labeler
   namespace: default
-  
 spec:
-  serviceAccountName: node-labeler-sa
+  serviceAccountName: node-labeler-sa  # <-- Ensure the ServiceAccount is properly used
   tolerations:
   - key: "node-role.kubernetes.io/control-plane"
     operator: "Exists"
-    effect: "NoSchedule"
-  
+    effect: "NoSchedule"  # Tolerate control-plane taint
   containers:
   - name: label-nodes
     image: bitnami/kubectl:latest
-    command:
-      - /bin/bash
-      - -c
+    command: ["/bin/bash", "-c"]
     args:
       - |
         while true; do
           echo "Checking and labeling nodes..."
           for node in \$(kubectl get nodes --no-headers | awk '{print \$1}'); do
-            control_plane_taint=\$(kubectl get node \$node --output=jsonpath='{.metadata.labels["node-role.kubernetes.io/control-plane"]}')
-            if [[ "$MODIFY_CONTROL_PLANE" == "false" && "\$control_plane_taint" == "true" ]]; then
-              echo "Skipping control-plane node: \$node"
-              continue
-            fi
-
-            if [[ \$(kubectl get node \$node --output=jsonpath='{.metadata.labels["node-role.kubernetes.io/worker"]}') == "" ]]; then
-              kubectl label node \$node node-role.kubernetes.io/worker= --overwrite
-              echo "Labeled node \$node as worker."
+            if [[ \$(kubectl get node \$node --output=jsonpath='{.metadata.labels.node-role\.kubernetes\.io/master}') != "true" ]]; then
+              if [[ \$(kubectl get node \$node --output=jsonpath='{.metadata.labels.node-role\.kubernetes\.io/worker}') == "" ]]; then
+                kubectl label node \$node node-role.kubernetes.io/worker=
+                echo "Labeled node \$node as worker."
+              fi
             fi
           done
-          echo "Sleeping for $LISTENER_DEFAULT_TIMER seconds..."
-          sleep $LISTENER_DEFAULT_TIMER
+          echo "Sleeping for 1 minute..."
+          sleep 60
         done
   restartPolicy: Always
 EOF
@@ -566,12 +541,18 @@ apply_node_listener() {
 	echo_message SUCCESS "$LISTENER_NODE applied successfully."
 }
 
+taint_nodes() {
+	echo_message INFO "Tainting current nodes..."
+	sudo kubectl taint nodes $(hostname) node-role.kubernetes.io/control-plane:NoSchedule --overwrite=true > /dev/null
+	echo_message SUCCESS "Tainting completed."
+}
+
 install_helm() {
     install_curl_package() {
         sudo apt-get update > /dev/null 2>&1
         sudo apt-get install -y curl > /dev/null 2>&1
     }
-    
+
     install_git_package() {
         sudo apt-get update > /dev/null 2>&1
         sudo apt-get install -y git > /dev/null 2>&1
@@ -582,7 +563,7 @@ install_helm() {
 
     echo -e "$(printf '=%.0s' {1..100})"
 	echo_message INFO "Installing Helm..."
-    
+
     if ! command -v curl > /dev/null 2>&1; then
 		echo_message INFO "curl is not installed. Installing curl..."
         install_curl_package 
@@ -597,28 +578,28 @@ install_helm() {
 
     if curl -fsSL -o "$helm_install_script" "$helm_install_script_url" > /dev/null; then
         chmod +x "$helm_install_script"
-        
+
         if ./"$helm_install_script" > /dev/null; then
             if helm version >/dev/null 2>&1; then
                 rm -f "$helm_install_script"
                 return 0
-				
+
             else
 				echo_message ERROR "Helm installation failed during verification."
                 return 1
-				
+
             fi
-			
+
         else
 			echo_message ERROR "Helm installation script failed during verification."
             return 1
-			
+
         fi
-		
+
     else
 		echo_message ERROR "Helm installation script failed to download."
         return 1
-		
+
     fi
 }
 
@@ -628,11 +609,11 @@ manage_helm() {
         echo_message DEBUG "Helm is already installed."
         return 0
     fi
-		
+
 	if [ "$HELM_ONLY" = true ]; then
 		install_helm
 		helm_install_status=$?
-	
+
 		if [ $helm_install_status -eq 0 ]; then
 			HELM_VER=$(helm version --short 2>/dev/null)
 			if [ -n "$HELM_VER" ]; then
@@ -645,18 +626,18 @@ manage_helm() {
 				exit 1
 
 			fi
-			
+
 		else
 			echo_message ERROR "Helm installation failed."
 			exit 1
-			
+
 		fi
 	fi
-	
+
     if [ "$INSTALL_HELM" = true ]; then
         install_helm
         helm_install_status=$?
-        
+
         if [ $helm_install_status -eq 0 ]; then
 			HELM_VER=$(helm version --short 2>/dev/null)
 			if [ -n "$HELM_VER" ]; then
@@ -670,19 +651,13 @@ manage_helm() {
 
 			fi
 		fi
-		
+
     else
 		echo -e "$(printf '=%.0s' {1..100})\n"
         echo_message DEBUG "Helm installation was skipped by the user."
         return 2
-		
-    fi
-}
 
-taint_nodes() {
-	echo_message INFO "Tainting nodes..."
-	sudo kubectl taint nodes $(hostname) node-role.kubernetes.io/control-plane:NoSchedule --overwrite=true
-	echo_message SUCCESS "Nodes tainted successfully."
+    fi
 }
 
 manage_worker_installation() {
@@ -711,67 +686,64 @@ manage_master_installation() {
 	display
 }
 
-manage_pre_run() {
+
+main() {
 	if [[ -f /etc/os-release ]]; then
 		. /etc/os-release
 		OS_NAME=$NAME
 		OS_VERSION=$VERSION_ID
 		echo_message DEBUG "Detected OS: $OS_NAME $OS_VERSION"
-		
+
 	else
 		echo_message ERROR "Unable to detect OS version."
 		exit 1
-		
+
 	fi
 
 	if [[ "$OS_NAME" = "Debian GNU/Linux" || "$OS_NAME" = "Ubuntu" ]]; then
 		if [[ "$OS_VERSION" != "11" && "$OS_VERSION" != "12" && "$OS_VERSION" != "22.04" && "$OS_VERSION" != "24.04" ]]; then
 			echo_message ERROR "Unsupported OS version: $OS_NAME $OS_VERSION"
 			exit 1
-			
+
 		fi
-		
+
 	else
 		echo_message ERROR "Unsupported OS: $OS_NAME"
 		exit 1
-		
+
 	fi
-	
+
 	if [ "$HELM_ONLY" = true ]; then
 		manage_helm
-	
-	fi
-}
 
-main() {
-	manage_pre_run
-	
+	fi
+
 	if [ "$IS_MASTER" = true ]; then
 		manage_worker_installation
 		manage_master_installation
 		manage_helm
-		
+
+
 		echo -e "$(printf '=%.0s' {1..100})\n"
 		printf "Use the following command to create new joining commands:\n"
 		printf "sudo kubeadm token create --print-join-command\n\n"
 		echo ""
-		
+
 		exit 0
-	
+
 	else
 		manage_worker_installation
 		manage_helm
-		
+
 		echo -e "$(printf '=%.0s' {1..100}\n)"
 		echo_message DEBUG "Run the following command in the master machine:"
 		echo_message DEBUG "sudo kubeadm token create --print-join-command"
 		echo_message DEBUG ""
 		echo_message DEBUG "Copy the output and run it on the agent machine."
 		echo ""
-		
+
 		exit 0
 	fi
 }
-
 
 main
